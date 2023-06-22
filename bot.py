57b733180d8e9666609519f1810d3f9cb4bc4f44
tarun gowda <tarun.gowda2000@gmail.com>
2023-06-22 13:33:08 +0530

    frontend barebone

--- bot.py
+++ bot.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 def main():
-  st.write('check')
+  st.set_page_config(page_title="PDF Bot", page_icon=":robot_face:")
+
+  st.header("Chat with multiple pdfs")
+  st.text_input("Ask a question about your documents:")
+
+  with st.sidebar:
+    st.subheader("Your documents")
+    pdfs = st.file_uploader("Upload your PDFs here and click 'Upload to DB'", accept_multiple_files=True)
+    st.button("Upload to DB")
 
 if __name__ == '__main__':
   main()
