3ead185d091fa45d174cdd21743a4f19edbd991a
tarun gowda <tarun.gowda2000@gmail.com>
2023-06-29 22:49:33 +0530

    respond to message on press of submit

--- bot.py
+++ bot.py
@@ -1,7 +1,46 @@
 import streamlit as st
+import os
+import chromadb
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from ingest import create_vector_db
 from chain import get_conversation_chain
 from chat_ui import message_display
+
+def load_chain():
+  collection_name = 'pdf_data'
+  dir_name = 'db'
+
+  if not os.path.exists(dir_name):
+    raise Exception(f"{dir_name} does not exist, nothing can be queried")
+
+  client_settings = chromadb.config.Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory=dir_name,
+    anonymized_telemetry=False
+  )
+
+  embeddings = OpenAIEmbeddings()
+
+  db = Chroma(
+    collection_name=collection_name,
+    embedding_function=embeddings,
+    client_settings=client_settings,
+    persist_directory=dir_name,
+  )
+
+  return get_conversation_chain(db)
+
+chain = load_chain()
+
+def execute_chain(query):
+  chain_result = None
+  try:
+      chain_result = chain(query)
+  except Exception as error:
+      print("error", error)
+
+  return chain_result
 
 def main():
   st.set_page_config(
@@ -34,19 +73,41 @@ def main():
   if "messages" not in st.session_state:
     st.session_state["messages"] = [("Hello! I'm a chatbot designed to help you with pdf documents.")]
 
+  c1, c2, c3 = st.columns([6.2, 1, 1])
+  with c1:
+    query = st.text_input(
+      label="Query", 
+      key="input", 
+      value="", 
+      placeholder="Ask your question here...",
+      label_visibility = "collapsed"
+    )
+  
+  with c2:
+    submit_button = st.button("Submit")
+
+  with c3:
+    reset_button = st.button("Reset")
+
+  if len(query) > 2 and submit_button:
+    messages = st.session_state['messages']
+
+    result = execute_chain(query)
+
+    for i, message in enumerate(result['chat_history']):
+      if i % 2 == 0:
+        print("user:" + message.content)
+        st.session_state.past.append(message.content)
+      else:
+        messages.append((query, message.content))
+        st.session_state.generated.append(message.content)
+        print("bot:" + message.content)
+
   with messages_container:
     if st.session_state["generated"]:
       for i in range(len(st.session_state["generated"])):
         message_display(st.session_state["past"][i], is_user=True)
         message_display(st.session_state["generated"][i])
-
-  query = st.text_input(
-    label="Query", 
-    key="input", 
-    value="", 
-    placeholder="Ask your question here...",
-    label_visibility = "collapsed"
-  )
 
   with st.sidebar:
     st.subheader("Your documents")
